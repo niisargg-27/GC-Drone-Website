425c7a7a84952d786dbc34fb57b5d93caff099e9
Algo <shahajideshmukh003@gmail.com>
2023-03-18 01:50:52 +0530

    Comiting Home page

---
+++ textutils/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Home from './Components/Hero/Home';
+
+function App(){
+  return (
+<>
+<Home/>
+<Footer/>
+</>
+  );
+}
+
+export default App;
